a9d7461289c5fba40ba5c10ed2c576b25d3c7839
Srone Codes <40174034+SaranshChauhan@users.noreply.github.com>
2021-05-26 16:28:49 +0530

    Update neonNum4.rb

--- neonNum4.rb
+++ neonNum4.rb
@@ -1,4 +1,5 @@
 # A positive integer whose sum of digits of its square is equal to the number itself is called a neon number.
+# 9 is a Neon number because 9^2 = 81 & 8+1 = 9 i.e number. 
 
 puts "Please! Enter the Number."
 num = gets.to_i
@@ -7,7 +8,7 @@ num = num.abs
 temp = num
 
 def isNeon(num)
-    sqr = num * 2
+    sqr = num ** 2
     sum = 0
     while sqr != 0
         sum = sum + sqr % 10
