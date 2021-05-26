cbcba0d9875dda663ec1262e348226c335c04253
saranshchauhan <saranshchauhan700@gmail.com>
2021-05-26 13:49:23 +0530

    Neon Num 0,1,9 in 1 millons

---
+++ neonNum.rb
@@ -0,0 +1,21 @@
+# A positive integer whose sum of digits of its square is equal to the number itself is called a neon number.
+
+puts "Please! Enter the Number."
+num = gets.to_i
+
+num = num.abs
+temp = num
+
+def isNeon(num)
+    sqr = num * 2
+    sum = 0
+    while sqr != 0
+        sum = sum + sqr%10
+        sqr /= 10
+    end
+    
+    return sum
+end
+
+puts "Is #{num} a Neon Number?"
+puts isNeon(num) == temp ? "Yes" : "No"
