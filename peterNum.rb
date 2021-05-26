9fc442a06eedec00cb1ce4fa634b35dfffbaee45
saranshchauhan <saranshchauhan700@gmail.com>
2021-05-26 13:01:29 +0530

    update

---
+++ peterNum.rb
@@ -0,0 +1,21 @@
+=begin
+
+A number is said to be Peterson if the sum of factorials of 
+each digit is equal to the sum of the number itself.
+
+INPUT : 145
+OUTPUT : Yes
+145 = !1 + !4 + !5 = 1 + 24 + 120 = 145
+
+=end
+
+
+def fact(num)
+    
+end
+
+puts "Enter a number!"
+num = gets.to_i
+
+
+
